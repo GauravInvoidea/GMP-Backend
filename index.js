53c471ea8cf352602fdbbe4a3e2a2e844868b9c7
GauravInvoidea <gauravinvoidea@gmail.com>
2024-08-07 14:11:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const userRoutes = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const locationRoutes = require('./routes/locationRoutes');
-const venderRoute = require('./routes/venderRoutes');
+// const venderRoute = require('./routes/venderRoutes');
 
 app.use(cors());
 app.use(morgan('dev'))
@@ -22,7 +22,7 @@ app.use('/gmp/file', express.static("public/property_files"));
 app.use('/gmp/image', express.static("public/images"));
 
 app.use('/gmp/api/admin', adminRoutes);
-app.use('/gmp/api/vender', venderRoute);
+// app.use('/gmp/api/vender', venderRoute);
 app.use('/gmp/api/user', userRoutes);
 app.use('/gmp/api/location', locationRoutes);
 
