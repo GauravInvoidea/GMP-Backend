cdf313a81afb7cdaea9fe8ad873d621a725f31a6
GauravInvoidea <gauravinvoidea@gmail.com>
2024-08-07 13:15:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 require('dotenv').config();
 const PORT = process.env.PORT;
-const app = require('./app');
 const connectToDB = require("./config/db");
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+
+// const unspecifiedRouteHandler = require('./routes/unspecifiedRouteHandler.js');
+const { finalErrorHandler } = require('./errorHandler/apiErrorHandler');
+const userRoutes = require('./routes/userRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const locationRoutes = require('./routes/locationRoutes');
+const venderRoute = require('./routes/venderRoutes');
+
+app.use(cors());
+app.use(morgan('dev'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/gmp/file', express.static("public/property_files"));
+app.use('/gmp/image', express.static("public/images"));
+
+app.use('/gmp/api/admin', adminRoutes);
+app.use('/gmp/api/vender', venderRoute);
+app.use('/gmp/api/user', userRoutes);
+app.use('/gmp/api/location', locationRoutes);
+
+app.get('/gmp/api/test', (req, res) => res.status(200).json({ message: 'test api' }));
+
+// app.use(unspecifiedRouteHandler);
+app.use(finalErrorHandler);
 
 const initialiseServer = async () => {
     try {
