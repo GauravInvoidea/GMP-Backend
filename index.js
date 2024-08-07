a0ae7a182e3d3f5adfa5a38161bc03620acc95a0
GauravInvoidea <gauravinvoidea@gmail.com>
2024-08-07 13:27:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const app = express();
 
 // const unspecifiedRouteHandler = require('./routes/unspecifiedRouteHandler.js');
 //const { finalErrorHandler } = require('./errorHandler/apiErrorHandler');
-//const userRoutes = require('./routes/userRoutes');
-//const adminRoutes = require('./routes/adminRoutes');
-//const locationRoutes = require('./routes/locationRoutes');
-//const venderRoute = require('./routes/venderRoutes');
+const userRoutes = require('./routes/userRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const locationRoutes = require('./routes/locationRoutes');
+const venderRoute = require('./routes/venderRoutes');
 
 app.use(cors());
 app.use(morgan('dev'))
@@ -21,10 +21,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/gmp/file', express.static("public/property_files"));
 app.use('/gmp/image', express.static("public/images"));
 
-//app.use('/gmp/api/admin', adminRoutes);
-//app.use('/gmp/api/vender', venderRoute);
-//app.use('/gmp/api/user', userRoutes);
-//app.use('/gmp/api/location', locationRoutes);
+app.use('/gmp/api/admin', adminRoutes);
+app.use('/gmp/api/vender', venderRoute);
+app.use('/gmp/api/user', userRoutes);
+app.use('/gmp/api/location', locationRoutes);
 
 app.get('/gmp/api/test', (req, res) => res.status(200).json({ message: 'test api' }));
 
