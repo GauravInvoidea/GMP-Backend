79e1c30266b0cb96dfb3d478272bcc49bf02b96d
GauravInvoidea <gauravinvoidea@gmail.com>
2024-08-08 14:28:04 +0530

    Delete routes directory

--- routes/unspecifiedRouteHandler.js
+++
@@ -1,5 +0,0 @@
-const unspecifiedRouteHandler = async (req, res) => {
-	res.status(404).json({ status: false, message: 'route / method not found! please check url' });
-}
-
-module.exports = unspecifiedRouteHandler;
